1c325d2a6ef83dc536e7facb31306fff14d752de
MasonD-007 <masondrake305@gmail.com>
2023-10-04 13:09:30 -0700

    10/4

--- Session_1_Prompt.py
+++ Session_1_Prompt.py
@@ -6,4 +6,4 @@ and two sentences about your favorite movie and your favorite food.
 if __name__ == "__main__":
     print("My name is: Mason Drake")
     print("My favorite movie is: Avengers: Endgame. Because it merges all of the MCU into one movie.")
-    print("My favorite food is: Pasta. Because I love the taste of it.") #kinda boring 
+    print("My favorite food is: Pasta. Because I love the taste of it.")
