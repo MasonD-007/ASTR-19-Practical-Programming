d585a7f609f88524a7127abb20ad1dfd6e9366a7
MasonD-007 <masondrake305@gmail.com>
2023-10-23 14:46:33 -0700

    10/23

--- Live_Demonstrations/Unit_1_Code.py
+++ Live_Demonstrations/Unit_1_Code.py
@@ -1,11 +1,13 @@
 import sys
 import numpy as np
 
+
 def main():
     x = 0.0
     for i in range(20):
-        x = float((i*2) + 19)
-        print(f'The value of x is = {x}')
+        x = float((i * 2) + 19)
+        print(f"The value of x is = {x}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
