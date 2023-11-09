41c95517c10d3e595260a05cad2ffdab7c692aad
MasonD-007 <masondrake305@gmail.com>
2023-11-09 12:26:38 -0800

    11/9

--- ClassFiles/test.py
+++ ClassFiles/test.py
@@ -1,20 +1,28 @@
-import matplotlib.pyplot as plt
-from matplotlib import patches
+import numpy as np
+from scipy.stats import zscore
 
-# how to make a circle in matplotlib
-
-circle1 = plt.Circle((2, 2), 0.2, color="r")
-rectangle = patches.Rectangle(
-    (1, 1), 2, 2, linewidth=1, facecolor="none", color="white"
+# Define the prior measurements
+x = np.array(
+    [
+        1.40796533,
+        0.29104673,
+        1.74539197,
+        -0.17648601,
+        0.60321751,
+        2.90038798,
+        2.06495026,
+        1.37243758,
+        0.93980907,
+        -0.04161072,
+    ]
 )
-background = patches.Rectangle((0, 0), 4, 4, color="black")
 
-# show the circle
-fig, ax = plt.subplots()
-fig.set_visible(True)
-ax.add_artist(background)
-ax.add_artist(rectangle)
-ax.add_artist(circle1)
-ax.set_xlim((0, 4))
-ax.set_ylim((0, 4))
-plt.show()
+# Calculate the mean and standard deviation of the prior measurements
+mean = np.mean(x)
+std = np.std(x, ddof=1)
+
+# Calculate the z-score of the new measurement
+new_measurement = 10.0
+z = (new_measurement - mean) / std
+
+print("The z-score of the new measurement is:", z)
